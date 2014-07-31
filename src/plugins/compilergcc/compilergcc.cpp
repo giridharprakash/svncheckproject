giridharprakash/svncheckproject

commit 893cff68310a7824d396d94d7baa295013dddf91

* compiler: rate limit build message column auto fit frequency to reduce excessive overhead when builds produce thousands of messages
git-svn-id: http://svn.code.sf.net/p/codeblocks/code/trunk@9852 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/plugins/compilergcc/compilergcc.cpp
@@ -3467,7 +3467,16 @@ void CompilerGCC::LogWarningOrError(CompilerLineType lt, cbProject* prj, const w
     Logger::level lv = (lt == cltError)   ? Logger::error
                      : (lt == cltWarning) ? Logger::warning : Logger::info;
 
-    m_pListLog->Append(errors, lv, 2);
+    // when there are many lines (thousands) of output, auto fitting column width
+    // is very expensive, so rate limit it to a maximum of 1 fit per 3 seconds
+    static wxDateTime lastAutofitTime = wxDefaultDateTime;
+    if ( lastAutofitTime < (wxDateTime::Now() - wxTimeSpan::Seconds(3)) )
+    {
+        lastAutofitTime = wxDateTime::Now();
+        m_pListLog->Append(errors, lv, 2); // auto fit the 'Message' column
+    }
+    else
+        m_pListLog->Append(errors, lv);
 
     // add to error keeping struct
     m_Errors.AddError(lt, prj, filename, line.IsEmpty() ? 0 : atoi(wxSafeConvertWX2MB(line)), msg);

commit 0b4d8d4c92858419dd2d0bb0b74dd6edee97c473

- wx30: Fix an assert in the compiler plugin introduced in r9852 (thanks muumi)
git-svn-id: http://svn.code.sf.net/p/codeblocks/code/trunk@9854 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/plugins/compilergcc/compilergcc.cpp
@@ -3469,7 +3469,7 @@ void CompilerGCC::LogWarningOrError(CompilerLineType lt, cbProject* prj, const w
 
     // when there are many lines (thousands) of output, auto fitting column width
     // is very expensive, so rate limit it to a maximum of 1 fit per 3 seconds
-    static wxDateTime lastAutofitTime = wxDefaultDateTime;
+    static wxDateTime lastAutofitTime = wxDateTime((time_t)0);
     if ( lastAutofitTime < (wxDateTime::Now() - wxTimeSpan::Seconds(3)) )
     {
         lastAutofitTime = wxDateTime::Now();